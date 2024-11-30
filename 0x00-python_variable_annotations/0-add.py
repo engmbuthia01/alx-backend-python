dd65b97ad3b9a11efcda60938adf9e5967a3cf78
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-11-30 11:02:14 +0000

    add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module containing a function that adds
+two float numbers and returns a float number
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers
+    """
+    return a + b

ad064770b9516469ffe61e9936a7d323aedbe358
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-11-30 11:34:57 +0000

    string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module contains a function that returns
+a string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns a string representation of a float"""
+    return str(n)

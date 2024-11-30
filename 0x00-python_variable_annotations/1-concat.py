ef7c79685182d4a8bf503a39eedf2d8234a33cb6
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-11-30 11:07:08 +0000

    concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module containing a function that
+concanates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings"""
+    return str1 + str2
